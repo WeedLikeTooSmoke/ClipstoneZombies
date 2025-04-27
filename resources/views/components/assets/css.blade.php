2f2374ffe4d4ea7f3100693dff2962861a70673c
DDDDec <declandavidwww@gmail.com>
2025-04-27 15:43:40 +0100

    Finish environment setup
    Finish environment setup

--- resources/views/components/assets/css.blade.php
+++ resources/views/components/assets/css.blade.php
@@ -1,4 +1,12 @@
 <link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}"/>
 <link rel="stylesheet" href="{{ asset('assets/css/root.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/grid.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/flex.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/animations.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/default.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/import.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/queries.css') }}"/>
+<link rel="stylesheet" href="{{ asset('assets/css/zombies.css') }}"/>
 
 @livewireStyles
