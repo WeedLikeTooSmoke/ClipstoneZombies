21ba7554e56e54ebba7578043219911bba3f5348
DDDDec <declandavidwww@gmail.com>
2025-04-28 22:29:29 +0100

    Add global header

--- resources/views/components/global/header.blade.php
+++ resources/views/components/global/header.blade.php
@@ -1,3 +1,12 @@
 <header>
-    <img class="header" src="{{ asset('assets/images/header.jpg') }}"/>
+    <img src="{{ asset('assets/images/header.jpg') }}"/>
 </header>
+<div class="header">
+    <h1>Clipstone Zombies Homepage</h1>
+    <div class="header-description">
+        Welcome to Clipstone Zombies homepage, We host multiple zombie servers for the Plutonium Call Of Duty modded <br>client from game titles like World At War, Black Ops 1 & Black Ops 2.
+    </div>
+    <div class="header-statistics">
+        <i class="fa-solid fa-circle pulse"></i> &nbsp;&nbsp;&nbsp;30 Players killing zombies
+    </div>
+</div>
--- resources/views/components/global/navigator.blade.php
+++ resources/views/components/global/navigator.blade.php
@@ -9,6 +9,7 @@
                 <a>Homepage <i class="fa-solid fa-chevron-up"></i></a>
                 <div class="dropdown-items">
                     <a><i class="fa-solid fa-house"></i> Homepage</a>
+                    <a><i class="fa-solid fa-newspaper"></i> Newspaper</a>
                     <div class="divider"></div>
                     <a><i class="fa-brands fa-square-facebook"></i> Facebook Page</a>
                     <a><i class="fa-brands fa-youtube"></i> YouTube Channel</a>
@@ -16,9 +17,34 @@
                     <a><i class="fa-brands fa-reddit-alien"></i> Reddit Page</a>
                 </div>
             </div>
-            <a>Community <i class="fa-solid fa-chevron-up"></i></a>
-            <a>Store <i class="fa-solid fa-chevron-up"></i></a>
-            <a>Account <i class="fa-solid fa-chevron-up"></i></a>
+
+            <div class="dropdown">
+                <a>Community <i class="fa-solid fa-chevron-up"></i></a>
+                <div class="dropdown-items">
+                    <a><i class="fa-solid fa-magnifying-glass"></i> Search Players</a>
+                    <div class="divider"></div>
+                    <a><i class="fa-solid fa-chart-simple"></i> Round Leaderboard</a>
+                    <a><i class="fa-solid fa-chart-simple"></i> Stats Leaderboard</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <a>Store <i class="fa-solid fa-chevron-up"></i></a>
+                <div class="dropdown-items">
+                    <a><i class="fa-solid fa-palette"></i> Username Color</a>
+                    <a><i class="fa-solid fa-star"></i> Player Ranks</a>
+                    <div class="divider"></div>
+                    <a><i class="fa-solid fa-money-bill"></i> Bank Money</a>
+                </div>
+            </div>
+
+            <div class="dropdown">
+                <a>Account <i class="fa-solid fa-chevron-up"></i></a>
+                <div class="dropdown-items">
+                    <a><i class="fa-solid fa-right-to-bracket"></i> Login</a>
+                    <a><i class="fa-solid fa-user-plus"></i> Register</a>
+                </div>
+            </div>
         </div>
     </nav>
 </div>
