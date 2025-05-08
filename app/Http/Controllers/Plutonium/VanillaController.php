ca386bc01117e2cee6afc3a9fa65edb5387fcc34
DDDDec <declandavidwww@gmail.com>
2025-05-09 00:15:55 +0100

    Update Controller and api
    Update Controller and api

---
+++ app/Http/Controllers/Plutonium/VanillaController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Plutonium;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use App\Models\User;
+
+class VanillaController extends Controller
+{
+    public function account(Request $request)
+    {
+        $data = $request->only([
+            'guid',
+            'name',
+        ]);
+
+        $player = User::where('guid', '=', '3390756')->get();
+
+        if ($player->count() == 0) {
+            return response()->json([
+                'guid' => 0,
+                'name' => 0,
+                'verified' => 0,
+                'rank' => 0,
+                'level' => 0,
+                'banned' => 0,
+                'color' => 0,
+            ]);
+        } else {
+            if ($player[0]->email_verified_at == null) { $verified = 0; } else { $verified = 1; }
+
+            return response()->json([
+                'guid' => $player[0]->guid,
+                'name' => $player[0]->name,
+                'verified' => $verified,
+                'rank' => $player[0]->rank,
+                'level' => $player[0]->level,
+                'banned' => $player[0]->banned,
+                'color' => $player[0]->color,
+                'welcome' = [
+                    "-------------[ ^5Clipstone Zombies^7 ]-------------",
+                    "Welcome to Clipstone Zombies. Play fair and enjoy the servers",
+                    "Please read the rules to be sure you're not breaking them",
+                    "-------------[ ^5Clipstone Zombies^7 ]-------------",
+                ]
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 include_once __DIR__.'/routing/pages.php';
 include_once __DIR__.'/routing/auth/auth.php';
 include_once __DIR__.'/routing/auth/email.php';
+include_once __DIR__.'/routing/api/plutonium/vanilla.php';
