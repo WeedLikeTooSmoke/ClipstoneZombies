8c7f34d2e86c20e8788807910671e56f3cdfce38
DDDDec <declandavidwww@gmail.com>
2025-05-08 18:50:21 +0100

    Login Frontend
    Login Frontend

--- resources/views/components/login/login.blade.php
+++ resources/views/components/login/login.blade.php
@@ -1,5 +1,11 @@
 <div class="container page">
     <div class="login">
+        <label><i class="fa-solid fa-user"></i> Username</label><br>
+        <input type="text" name="name" placeholder="Username..."/><br><br><br>
 
+        <label><i class="fa-solid fa-lock"></i> Password</label><br>
+        <input type="password" name="password" placeholder="Password..."/><br><br>
+
+        <button type="submit">Login</button>
     </div>
 </div>
