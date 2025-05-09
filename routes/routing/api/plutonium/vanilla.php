a0b996bb6833af6008cb4cb0eba6dbe48251bfa8
DDDDec <declandavidwww@gmail.com>
2025-05-09 17:53:39 +0100

    Add api routes for leaderboard and stats
    Add api routes for leaderboard and stats

--- routes/routing/api/plutonium/vanilla.php
+++ routes/routing/api/plutonium/vanilla.php
@@ -3,3 +3,5 @@
 use App\Http\Controllers\Plutonium\VanillaController;
 
 Route::post('/api/vanilla/account', [VanillaController::class, 'account'])->name('api.vanilla.account');
+Route::post('/api/vanilla/leaderboard', [VanillaController::class, 'leaderboard'])->name('api.vanilla.leaderboard');
+Route::post('/api/vanilla/stats', [VanillaController::class, 'stats'])->name('api.vanilla.stats');
