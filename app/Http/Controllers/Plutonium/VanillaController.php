69859e656f8d4ff183863d3dfe6dadbbe32e0729
DDDDec <declandavidwww@gmail.com>
2025-05-11 05:04:18 +0100

    Some fixes and changes
    Some fixes and changes

--- app/Http/Controllers/Plutonium/VanillaController.php
+++ app/Http/Controllers/Plutonium/VanillaController.php
@@ -22,23 +22,16 @@ class VanillaController extends Controller
     public function account(Request $request)
     {
         // Get only the data we want from the request
-        $data = $request->only([
-            'guid',
-            'name',
-        ]);
+        $data = $request->only(['guid', 'name']);
 
         // Get the player who joined the server
-        $player = User::where('guid', '=', $data['guid'])->get();
+        $player = User::where('guid', $data['guid'])->first();
 
         // Player does not have an account
-        if ($player->count() == 0)
+        if (!$player)
         {
             // Add player to joins table for register
-            $addJoin = Join::updateOrCreate([
-                'guid' => $data['guid'],
-            ],[
-                'name' => $data['name'],
-            ]);
+            $addJoin = Join::updateOrCreate(['guid' => $data['guid']],['name' => $data['name']]);
 
             // Return not registered json data
             return response()->json([
@@ -53,41 +46,32 @@ class VanillaController extends Controller
             ]);
         }
 
-        // Player has an account
-        if ($player->count() > 0)
-        {
-            // Check if player has verified email
-            if ($player[0]->email_verified_at == null) { $verified = 0; } else { $verified = 1; }
+        // Check if player has verified email
+        if ($player->email_verified_at == null) { $verified = 0; } else { $verified = 1; }
 
-            // Return registered json data
-            return response()->json([
-                'account-guid' => $player[0]->guid,
-                'account-name' => $player[0]->name,
-                'account-display-name' => "[^{$player[0]->color}". self::levelType(config('plutonium.settings.level_type'), $player[0]->level) ."^7][^{$player[0]->color}". self::rankType(config('plutonium.settings.rank_type'), $player[0]->rank) ."^7] ^{$player[0]->color}{$player[0]->name}",
-                'account-verified' => $verified,
-                'account-rank' => $player[0]->rank,
-                'account-level' => $player[0]->level,
-                'account-banned' => $player[0]->banned,
-                'account-color' => $player[0]->color,
-                'account-welcome' => [
-                    "-------------[ ^2Clipstone Zombies^7 ]-------------",
-                    "Welcome to ^2Clipstone Zombies^7. Play fair and enjoy the servers",
-                    "Please read the rules to be sure you're not breaking them",
-                    "-------------[ ^2Clipstone Zombies^7 ]-------------",
-                ]
-            ]);
-        }
+        // Return registered json data
+        return response()->json([
+            'account-guid' => $player->guid,
+            'account-name' => $player->name,
+            'account-display-name' => "[^{$player->color}". self::levelType(config('plutonium.settings.level_type'), $player->level) ."^7][^{$player->color}". self::rankType(config('plutonium.settings.rank_type'), $player->rank) ."^7] ^{$player->color}{$player->name}",
+            'account-verified' => $verified,
+            'account-rank' => $player->rank,
+            'account-level' => $player->level,
+            'account-banned' => $player->banned,
+            'account-color' => $player->color,
+            'account-welcome' => [
+                 "-------------[ ^2Clipstone Zombies^7 ]-------------",
+                "Welcome to ^2Clipstone Zombies^7. Play fair and enjoy the servers",
+                "Please read the rules to be sure you're not breaking them",
+                "-------------[ ^2Clipstone Zombies^7 ]-------------",
+            ]
+        ]);
     }
 
     public function leaderboards(Request $request)
     {
         // Get only the data we want from the request
-        $data = $request->only([
-            'map',
-            'players',
-            'players_count',
-            'round',
-        ]);
+        $data = $request->only(['map', 'players', 'players_count', 'round']);
         $data["gamemode"] = "Vanilla";
 
         // Create player('s') record in the leaderboards table
@@ -125,24 +109,24 @@ class VanillaController extends Controller
         ]);
 
         // Get the player who joined the server
-        $player = UsersStats::where('guid', '=', $data['guid'])->get();
+        $player = UsersStats::where('guid', $data['guid'])->first();
 
         // Save stats sent from the server
         $saveStats = UsersStats::updateOrCreate([
             'guid' => $data['guid'],
         ],[
-            'score' => $data['score'] + $player[0]->score,
-            'kills' => $data['kills'] + $player[0]->kills,
-            'downs' => $data['downs'] + $player[0]->downs,
-            'deaths' => $data['deaths'] + $player[0]->deaths,
-            'suicides' => $data['suicides'] + $player[0]->suicides,
-            'revives' => $data['revives'] + $player[0]->revives,
-            'headshots' => $data['headshots'] + $player[0]->headshots,
-            'melee_kills' => $data['melee_kills'] + $player[0]->melee_kills,
-            'grenade_kills' => $data['grenade_kills'] + $player[0]->grenade_kills,
-            'total_shots' => $data['total_shots'] + $player[0]->total_shots,
-            'hits' => $data['hits'] + $player[0]->hits,
-            'sacrifices' => $data['sacrifices'] + $player[0]->sacrifices,
+            'score' => $data['score'] + $player->score,
+            'kills' => $data['kills'] + $player->kills,
+            'downs' => $data['downs'] + $player->downs,
+            'deaths' => $data['deaths'] + $player->deaths,
+            'suicides' => $data['suicides'] + $player->suicides,
+            'revives' => $data['revives'] + $player->revives,
+            'headshots' => $data['headshots'] + $player->headshots,
+            'melee_kills' => $data['melee_kills'] + $player->melee_kills,
+            'grenade_kills' => $data['grenade_kills'] + $player->grenade_kills,
+            'total_shots' => $data['total_shots'] + $player->total_shots,
+            'hits' => $data['hits'] + $player->hits,
+            'sacrifices' => $data['sacrifices'] + $player->sacrifices,
         ]);
 
         // Return success json result
@@ -154,18 +138,16 @@ class VanillaController extends Controller
     public function autoMessages(Request $request)
     {
         // Get only the data we want from the request
-        $data = $request->only([
-            'map',
-        ]);
+        $data = $request->only(['map']);
 
         // Get a random number to determine what kind of message we want
-        if (random_int(0, 1) == 0) {
+        if (random_int(0, 1) === 0) {
             // Get the highest round record from the leaderboards table
             $random = random_int(1, 4);
             $record = Leaderboard::orderBy('round', 'desc')->where('map', $data['map'])->where('gamemode', 'Vanilla')->where('players_count', $random)->first();
 
             // Check if the record result is null
-            if ($record == null) {
+            if (!$record) {
                 // Return highest round record json
                 return response()->json([
                     'result' => "[^2ClipstoneZombies^7]: ".ucfirst($data['map'])." ".self::roundType($random)." Record > ^2This record has not yet been set...",
