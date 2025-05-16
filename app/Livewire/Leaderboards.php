a5e778ad0bcdbe26247467a3c64e80d1caf5b14e
Dec <declandavidwww@gmail.com>
2025-05-16 23:56:46 +0100

    Setup stats for tables
    Setup stats for tables

--- app/Livewire/Leaderboards.php
+++ app/Livewire/Leaderboards.php
@@ -3,11 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
+use App\Models\Leaderboard;
 class Leaderboards extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.leaderboards');
+        return view('livewire.leaderboards', [
+            'records' => Leaderboard::orderBy('round', 'desc')->paginate(10),
+        ]);
     }
 }
--- app/Livewire/StatisticLeaderboards.php
+++ app/Livewire/StatisticLeaderboards.php
@@ -3,11 +3,19 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+
+use App\Models\UsersStats;
 
 class StatisticLeaderboards extends Component
 {
+    use WithPagination;
+
+    public $orderBy = 'score';
     public function render()
     {
-        return view('livewire.statistic-leaderboards');
+        return view('livewire.statistic-leaderboards', [
+            'records' => UsersStats::orderBy($this->orderBy, 'desc')->paginate(10),
+        ]);
     }
 }
