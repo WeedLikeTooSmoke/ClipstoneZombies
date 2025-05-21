65d436acac3e884e7cc6b69042a008da6c739966
Dec <122358528+WeedLikeTooSmoke@users.noreply.github.com>
2025-05-21 09:35:26 +0100

    Update plutonium.php

--- config/plutonium.php
+++ config/plutonium.php
@@ -47,6 +47,23 @@ return [
                 '[^2ClipstoneZombies^7]: Rule > Don\'t glitch, hack or cheat',
                 '[^2ClipstoneZombies^7]: Reddit > ^2reddit.com/r/ClipstoneZombie',
             ],
+            'rules' => [
+                '-------------[ ^2Clipstone Rules^7 ]-------------',
+                '[^2ClipstoneZombies^7]: Don\'t be a meanie',
+                '[^2ClipstoneZombies^7]: Don\'t say slurs',
+                '[^2ClipstoneZombies^7]: Don\'t block players',
+                '[^2ClipstoneZombies^7]: Don\'t hold rounds',
+                '[^2ClipstoneZombies^7]: Don\'t glitch, hack or cheat',
+                '-------------[ ^2Clipstone Rules^7 ]-------------',
+            ],
+            'help' => [
+                '---------[ ^2Clipstone Help Page 1^7 ]---------',
+                '[^2ClipstoneZombies^7]: .account > Show your account details',
+                '[^2ClipstoneZombies^7]: .leaderboard > Show top 5 leaderboard records',
+                '[^2ClipstoneZombies^7]: .rules > Show all Clipstone Zombies rules',
+                '[^2ClipstoneZombies^7]: .stats > Show all of your statistics',
+                '---------[ ^2Clipstone Help Page 1^7 ]---------',
+            ],
         ],
     ],
 
