384d2fa3426475d47f4b505658927586e81803cf
Dec <122358528+WeedLikeTooSmoke@users.noreply.github.com>
2025-05-22 12:00:04 +0100

    Create api.php

---
+++ config/api.php
@@ -0,0 +1,8 @@
+<?php 
+
+return [
+  'api' => [
+      'key' => '9fiyn9pyujm98ap,hn4g987paj89hm,87r9dhp9mg[h9hgr,g,r',
+      'agent' => 'Plutonium/ClipstoneZombies/BlackOps2/Zombies/Servers',
+  ],
+];
