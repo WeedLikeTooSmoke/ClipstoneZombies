227e16d7e4e16e9a4139b18a3ac2cffad99e1351
Dec <122358528+WeedLikeTooSmoke@users.noreply.github.com>
2025-05-22 12:01:05 +0100

    Update api.php

--- config/api.php
+++ config/api.php
@@ -1,8 +1,6 @@
 <?php 
 
 return [
-  'api' => [
-      'key' => '9fiyn9pyujm98ap,hn4g987paj89hm,87r9dhp9mg[h9hgr,g,r',
-      'agent' => 'Plutonium/ClipstoneZombies/BlackOps2/Zombies/Servers',
-  ],
+    'key' => '9fiyn9pyujm98ap,hn4g987paj89hm,87r9dhp9mg[h9hgr,g,r',
+    'agent' => 'Plutonium/ClipstoneZombies/BlackOps2/Zombies/Servers',
 ];
