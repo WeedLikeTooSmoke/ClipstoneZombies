22a9d02a8e79a9b79076b505ff02eb60d20e4d8e
Dec <declandavidwww@gmail.com>
2025-05-25 22:49:11 +0100

    Update console.blade.php

--- resources/views/filament/admin/pages/console.blade.php
+++ resources/views/filament/admin/pages/console.blade.php
@@ -1,3 +1,11 @@
 <x-filament-panels::page>
+    <?php
+        $tool = new q3tool("127.0.0.1", 4977);
 
+        $players = $tool->get_info("playerlist");
+        $player_num = $tool->get_info("players");
+
+        var_dump($players);
+        var_dump($player_num);
+    ?>
 </x-filament-panels::page>
