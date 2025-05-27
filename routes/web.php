634179287e1e6185d035df780abdac430d1dc89f
Dec <declandavidwww@gmail.com>
2025-05-27 14:33:32 +0100

    Rearange api route files
    Rearange api route files

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 include_once __DIR__.'/routing/pages.php';
 include_once __DIR__.'/routing/auth/auth.php';
 include_once __DIR__.'/routing/auth/email.php';
-include_once __DIR__.'/routing/api/plutonium/vanilla.php';
+include_once __DIR__.'/routing/api/vanilla.php';
 include_once __DIR__.'/routing/rcon/vanilla.php';
