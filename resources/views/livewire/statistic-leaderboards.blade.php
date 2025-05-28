5d825046c60c3f6c22a2e1e54426d6d4d0a42873
Dec <declandavidwww@gmail.com>
2025-05-28 01:46:19 +0100

    Update statistic-leaderboards.blade.php

--- resources/views/livewire/statistic-leaderboards.blade.php
+++ resources/views/livewire/statistic-leaderboards.blade.php
@@ -14,13 +14,13 @@
             @foreach($records as $record)
                 <tr>
                     <td>{{ $record->name }}</td>
-                    <td>£{{ $record->score }}</td>
-                    <td>{{ $record->kills }}</td>
-                    <td>{{ $record->downs }}</td>
-                    <td>{{ $record->deaths }}</td>
-                    <td>{{ $record->revives }}</td>
-                    <td>{{ $record->headshots }}</td>
-                    <td>{{ $record->bosses_killed }}</td>
+                    <td>£{{ Number::forHumans($record->score) }}</td>
+                    <td>{{ Number::format($record->kills) }}</td>
+                    <td>{{ Number::format($record->downs) }}</td>
+                    <td>{{ Number::format($record->deaths) }}</td>
+                    <td>{{ Number::format($record->revives) }}</td>
+                    <td>{{ Number::format($record->headshots) }}</td>
+                    <td>{{ Number::format($record->bosses_killed) }}</td>
                 </tr>
             @endforeach
         </table>
